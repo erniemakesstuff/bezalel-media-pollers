2597cf74a49b8ed5964e03bffe64a239a0cd0e16
Ernie Castro <erniemakesstuff@gmail.com>
2024-11-27 17:45:13 -0800

    cleanup health service call to polly

--- health_service.py
+++ health_service.py
@@ -1,11 +1,9 @@
 import logging
 import os
 from flask import Flask
-import clients.polly as polly
 
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
 @app.route("/health")
 def health_check():
-    polly.create_narration('testVocal.mp3', "Hello world", True)
     return "Healthy"
